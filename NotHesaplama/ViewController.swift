793e96158f6d546bd23c3fe2f133000deae482a0
Sevval Mertoglu <sevvalmertoglu131@gmail.com>
2023-02-27 22:56:37 +0300

    note calculation application completed

--- NotHesaplama/ViewController.swift
+++ NotHesaplama/ViewController.swift
@@ -9,11 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+   
+    @IBOutlet weak var midtermText: UITextField!
+    @IBOutlet weak var finalTxt: UITextField!
+    @IBOutlet weak var notelbl: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        notelbl.text = ""
     }
 
-
+    @IBAction func calculateNote(_ sender: Any) {
+        let midterm = Int(midtermText.text!)!
+        let final = Int(finalTxt.text!)!
+        
+        let note = (midterm + final) / 2
+        
+        notelbl.text = "\(note)"
+    }
+    
 }
 
